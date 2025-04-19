dde1c524f09f3df846225873b38886216e1b0fae
Matias Ariel Coco <cocomatias6@gmail.com>
2025-04-19 10:54:40 -0300

    prueba

---
+++ src/app/Parcial-2/Page.tsx
@@ -0,0 +1,10 @@
+import Preguntas from "@/components/Preguntas";
+
+export default function Home() {
+  return (
+    <>
+      <h1>Falta hacer lo del parcial 2</h1>
+      <Preguntas />
+    </>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import Preguntas from "@/components/Preguntas";
 export default function Home() {
   return (
     <>
+      <h1 className="text-white">Parcial 1:</h1>
       <Preguntas />
     </>
   );
