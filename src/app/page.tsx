bd43e9d5edc94a667952a2a2aeb7d145f369d24c
Matias Ariel Coco <91424961+matiasCoco1997@users.noreply.github.com>
2025-06-05 13:37:31 -0300

    fix parciales

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,44 @@
+"use client";
 import Preguntas from "@/components/Preguntas";
-import Link from "next/link";
+import PreguntasSegundo from "@/components/PreguntasSegundo";
+import { useState } from "react";
 
 export default function Home() {
+  const [parcialActivo, setParcialActivo] = useState<number>(1);
+
   return (
     <>
       <div className="flex items-center justify-between p-5 ">
-        <h1 className="text-white">Parcial 1</h1>
+        {parcialActivo === 1 ? (
+          <h1 className="text-white">Parcial 1</h1>
+        ) : (
+          <h1 className="text-white">Parcial 2</h1>
+        )}
 
-        <Link
-          href="segundo-parcial"
-          className="p-3 bg-blue rounded-lg bg-blue-500"
-        >
-          Parcial 2
-        </Link>
+        <div className="flex gap-2">
+          <button
+            onClick={() => {
+              setParcialActivo(1);
+            }}
+            className={`p-3 rounded-lg cursor-pointer ${
+              parcialActivo === 1 ? "bg-blue-600" : "bg-gray-500"
+            }`}
+          >
+            Parcial 1
+          </button>
+          <button
+            onClick={() => {
+              setParcialActivo(2);
+            }}
+            className={`p-3 rounded-lg cursor-pointer ${
+              parcialActivo === 2 ? "bg-blue-600" : "bg-gray-500"
+            }`}
+          >
+            Parcial 2
+          </button>
+        </div>
       </div>
-
-      <Preguntas />
+      {parcialActivo === 1 ? <Preguntas /> : <PreguntasSegundo />}
     </>
   );
 }
